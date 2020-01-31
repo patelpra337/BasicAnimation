01a7d053766b418898e9b02666666f71ed735ef7
Pravin <patelpra337@gmail.com>
2020-01-30 20:43:25 -0600

    Create new BasicAnimation project

---
+++ BasicAnimation/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  BasicAnimation
+//
+//  Created by patelpra on 1/30/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var label: UILabel!
+    private var stackView: UIStackView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureLabel()
+        configureStackView()
+    }
+    
+    private func configureLabel() {
+        
+    }
+    
+    private func configureStackView() {
+        
+    }
+
+
+}
+
